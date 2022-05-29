ed1902e54ba130b9807815951735e12762189565
loic-mnt <78222077+loic-mnt@users.noreply.github.com>
2022-05-29 23:26:29 +0200

    Add files via upload

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' @title
+#' The `language` dataset
+#'
+#' @description
+#' `languages` data provides the world "hello" in different languages
+#'
+#' @format
+#' This is a `data.frame` with `r nrow(language)` rows and `r ncol(language)` columns:
+#'
+#' *`code`: language code: "en","fr","it","es","de", for English, French, Italian, Spanish, and German
+#' *`hello`: word "hello" in the language corresponding to the code.
+#'
+#' @author Anouar El Ghouch <anouar.elghouch@uclouvain.be>
+#'
+#' @source
+#' <https://en.wikipedia.org/wiki/List_of_ISO_639-&_codes>
+#'
+#' <https://translate.google.com>
+#'
+"language"
